fe38cec4c466a5d0aff03c3f3888953a2b2e27fc
Miguel Rodriguez <7migro11@gmail.com>
2022-08-25 09:15:42 -0700

    added constructor for cityData and render function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Main from "./Main.js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "",
+      cityData: {},
+      error: false,
+      errorMesage: "",
+      showCard: true,
+    };
+  }
+
+  handleCity = (e) => {
+    let city = e.target.value;
+    this.setState({
+      city: city,
+    });
+  };
+
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.get(
+        `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+      );
+      console.log(response);
+      this.setState({
+        cityData: response.data[0],
+        mapURL: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${response.data[0].lat},${response.data[0].lon}&zoom=12`,
+        showCard: false,
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `Oh oh! It seems like an error has occured: ${error.response.status}`,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <Main
+          city={this.state.city}
+          cityData={this.state.cityData}
+          error={this.state.error}
+          errorMesage={this.state.errorMesage}
+          handleCity={this.handleCity}
+          handleCitySubmit={this.handleCitySubmit}
+          mapURL={this.state.mapURL}
+          showCard={this.state.showCard}
+        />
+      </>
+    );
+  }
 }
-
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.props.handleCitySubmit}>
+          <Form.Group>
+            <Form.Label>Enter a city name</Form.Label>
+            <Form.Control
+              name="city"
+              onInput={this.props.handleCity}
+              placeholder='"Seattle", "San Francisco"'
+              type="text"
+            ></Form.Control>
+            <Button type="submit" variant="primary">
+              Explore!
+            </Button>{" "}
+          </Form.Group>
+        </Form>
+        {this.props.showCard ? (
+          <p> </p>
+        ) : this.props.error ? (
+          <p>{this.props.errorMessage}</p>
+        ) : (
+          <Card>
+            <Card.Body>
+              <Card.Title>{this.props.cityData.display_name}</Card.Title>
+              <Card.Img variant="top" src={this.props.mapURL} />
+              <Card.Text></Card.Text>
+            </Card.Body>
+            <ListGroup>
+              <ListGroup.Item>'Lat: {this.props.cityData.lat}'</ListGroup.Item>
+              <ListGroup.Item>'Lon: {this.props.cityData.lon}'</ListGroup.Item>
+            </ListGroup>
+          </Card>
+        )}
+      </>
+    );
+  }
+}
+
+export default Main;
